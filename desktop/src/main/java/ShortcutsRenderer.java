6038eb1370ff44042ce9ce2e50837fdca466b381
Pooja <pooja.akshantal@gmail.com>
2012-08-31 18:13:35 +0530

    refactoring

--- desktop/src/main/java/ShortcutsRenderer.java
+++ desktop/src/main/java/ShortcutsRenderer.java
@@ -1,9 +1,83 @@
-/**
- * Created with IntelliJ IDEA.
- * User: poojaak
- * Date: 31/08/12
- * Time: 5:18 PM
- * To change this template use File | Settings | File Templates.
- */
-public class ShortcutsRenderer {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
+class ShortcutsRenderer extends JLabel implements ListCellRenderer {
+    private Font uhOhFont;
+
+    String[] imageNames = {"idea_icon.png", "idea_icon.png", "terminal.jpg", "eclipse_icon.png", "idea_icon.png"};
+    ImageIcon[] images = {};
+    String[] menuShortCuts = {"Idea Open File", "Idea Save File", "Terminal open", "Eclipse Open file", "Idea new file"};
+
+    public ShortcutsRenderer() {
+        setOpaque(true);
+        setHorizontalAlignment(LEFT);
+        setVerticalAlignment(CENTER);
+
+        images = new ImageIcon[menuShortCuts.length];
+        Integer[] intArray = new Integer[menuShortCuts.length];
+        for (int i = 0; i < menuShortCuts.length; i++) {
+            intArray[i] = new Integer(i);
+            images[i] = createImageIcon("covers/" + imageNames[i]);
+            if (images[i] != null) {
+                images[i].setDescription(menuShortCuts[i]);
+            }
+        }
+
+    }
+
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = Cat4Mouse.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    /*
+    * This method finds the image and text corresponding
+    * to the selected value and returns the label, set up
+    * to display the text and image.
+    */
+    public Component getListCellRendererComponent(
+            JList list,
+            Object value,
+            int index,
+            boolean isSelected,
+            boolean cellHasFocus) {
+        int selectedIndex = Arrays.asList(menuShortCuts).indexOf(value);
+
+        ImageIcon icon = images[selectedIndex];
+        String pet = menuShortCuts[selectedIndex];
+
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+
+        Image image = icon.getImage();
+        Image newImage = image.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(newImage));
+        if (icon != null) {
+            setUhOhText(pet, list.getFont());
+        } else {
+            setUhOhText(pet + " (no image available)", list.getFont());
+        }
+
+
+        return this;
+    }
+
+    //Set the font and text when no image was found.
+    protected void setUhOhText(String uhOhText, Font normalFont) {
+        if (uhOhFont == null) { //lazily create this font
+            uhOhFont = normalFont.deriveFont(Font.ITALIC);
+        }
+        setFont(uhOhFont);
+        setText(uhOhText);
+    }
 }
--- desktop/src/main/java/ShowAppNamePopup.java
+++ desktop/src/main/java/ShowAppNamePopup.java
@@ -4,31 +4,26 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.JTextComponent;
 import javax.swing.text.PlainDocument;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class ShowShortcutsPopup extends PlainDocument {
+public class ShowAppNamePopup extends PlainDocument {
     JComboBox comboBox;
     ComboBoxModel model;
     JTextComponent editor;
     // flag to indicate if setSelectedItem has been called
     // subsequent calls to remove/insertString should be ignored
-    boolean selecting=false;
+    boolean selecting = false;
+    private String enteredText="";
 
-    public ShowShortcutsPopup(final JComboBox comboBox) {
+    public ShowAppNamePopup(final JComboBox comboBox) {
         this.comboBox = comboBox;
         model = comboBox.getModel();
         editor = (JTextComponent) comboBox.getEditor().getEditorComponent();
-        comboBox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (!selecting) highlightCompletedText(0);
-            }
-        });
+
         editor.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
-                switch (e.getKeyCode()){
+                switch (e.getKeyCode()) {
                     case KeyEvent.VK_DOWN:
                     case KeyEvent.VK_UP:
                         break;
@@ -37,7 +32,12 @@ public class ShowShortcutsPopup extends PlainDocument {
                                 .getSource());
                         frame.setVisible(false);
                         break;
-
+                    case KeyEvent.VK_BACK_SPACE:
+                        deleteText();
+                    default :
+                        if(e.getKeyCode()>=65 && e.getKeyCode()<=90){
+                             comboBox.setPopupVisible(true);
+                        }
                 }
             }
         });
@@ -46,7 +46,11 @@ public class ShowShortcutsPopup extends PlainDocument {
     public void remove(int offs, int len) throws BadLocationException {
         // return immediately when selecting an item
         if (selecting) return;
-        super.remove(offs, len);
+       super.remove(offs, len);
+    }
+
+    private void deleteText() {
+        enteredText=enteredText.substring(0,enteredText.length()-1);
     }
 
     public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
@@ -59,16 +63,13 @@ public class ShowShortcutsPopup extends PlainDocument {
         if (item != null) {
             setSelectedItem(item);
         } else {
-            // keep old item selected if there is no match
-            item = comboBox.getSelectedItem();
-            // imitate no insert (later on offs will be incremented by str.length(): selection won't move forward)
-            offs = offs-str.length();
-            // provide feedback to the user that his input has been received but can not be accepted
-            comboBox.getToolkit().beep(); // when available use: UIManager.getLookAndFeel().provideErrorFeedback(comboBox);
+            comboBox.getSelectedItem();
+            offs = offs - str.length();
+            comboBox.getToolkit().beep();
         }
-        setText(item.toString());
+        enteredText+=str;
+        setText(enteredText);
         // select the completed part
-        highlightCompletedText(offs+str.length());
     }
 
     private void setText(String text) throws BadLocationException {
@@ -77,10 +78,6 @@ public class ShowShortcutsPopup extends PlainDocument {
         super.insertString(0, text, null);
     }
 
-    private void highlightCompletedText(int start) {
-        editor.setSelectionStart(start);
-        editor.setSelectionEnd(getLength());
-    }
 
     private void setSelectedItem(Object item) {
         selecting = true;
@@ -89,26 +86,26 @@ public class ShowShortcutsPopup extends PlainDocument {
     }
 
     private Object lookupItem(String pattern) {
-        Object selectedItem = model.getSelectedItem();
-        // only search for a different item if the currently selected does not match
-        if (selectedItem != null && startsWithIgnoreCase(selectedItem.toString(), pattern)) {
-            return selectedItem;
-        } else {
-            // iterate over all items
-
-            for (int i=0, n=model.getSize(); i < n; i++) {
-                Object currentItem = model.getElementAt(i);
-                // current item starts with the pattern?
-                if (startsWithIgnoreCase(currentItem.toString(), pattern)) {
-                    return currentItem;
-                }
-            }
+        if (startsWithIgnoreCase("i", pattern)) {
+            comboBox.removeAllItems();
+            comboBox.insertItemAt("Idea", 0);
+            comboBox.insertItemAt("IE", 1);
+            comboBox.insertItemAt("IE", 2);
+            comboBox.insertItemAt("IE", 3);
+            comboBox.insertItemAt("IE", 4);
+            comboBox.insertItemAt("IE", 5);
         }
-        // no item starts with the pattern => return null
+
+        if (startsWithIgnoreCase("t", pattern)) {
+            comboBox.removeAllItems();
+            comboBox.insertItemAt("Terminal", 0);
+        }
+        comboBox.setMaximumRowCount(comboBox.getItemCount() );
+        comboBox.setPopupVisible(true);
+
         return null;
     }
 
-    // checks if str1 starts with str2 - ignores case
     private boolean startsWithIgnoreCase(String str1, String str2) {
         return str1.toUpperCase().startsWith(str2.toUpperCase());
     }
